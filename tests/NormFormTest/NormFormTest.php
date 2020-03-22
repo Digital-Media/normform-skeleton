d123048d9da760961d8d6994c6da7a4f268e7520
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2020-03-22 05:28:35 +0100

    Updated tests
    Test cases are now PHPUnit 9.0 compatible and adhere to PSR-12.
    Script slightly simplified.

--- tests/NormFormTest/NormFormTest.php
+++ tests/NormFormTest/NormFormTest.php
@@ -2,17 +2,19 @@
 
 namespace NormFormTest;
 
-/**
- * Class for testing NormFormDemo with PHPUnit
- * If this class is called in a commandline with phpunit, all methods starting with test* are called in the given order
- * Use startNormFormTest.sh to do so
- */
 use NormFormSkeleton\NormFormDemo;
 use Fhooe\NormForm\Parameter\PostParameter;
 use Fhooe\NormForm\Parameter\GenericParameter;
 use Fhooe\NormForm\View\View;
+use PHPUnit\Framework\TestCase;
 
-class NormFormTest extends \PHPUnit\Framework\TestCase
+/**
+ * PHPUnit test cases for the NormFormDemo class.
+ * If this class is called in a commandline with PHPUnit, all functions starting with "test" are called in the given
+ * order. You can use startNormFormTest.sh to do just this.
+ * @package NormFormTest
+ */
+class NormFormTest extends TestCase
 {
     /**
      * The NormForm object.
@@ -29,9 +31,9 @@ class NormFormTest extends \PHPUnit\Framework\TestCase
     private $view;
 
     /**
-     * setUp() is run before each test and initializes the View and NormFormDemo object
+     * setUp() is run before each test and initializes the View and NormFormDemo object.
      */
-    public function setUp()
+    public function setUp(): void
     {
         $this->view = new View(
             "normFormDemo.html.twig",
@@ -49,7 +51,7 @@ class NormFormTest extends \PHPUnit\Framework\TestCase
     /**
      * This method is run after each test to free memory. Could be left empty to make PHP handle this.
      */
-    public function tearDown()
+    public function tearDown(): void
     {
         unset($this->normForm);
     }
@@ -58,7 +60,7 @@ class NormFormTest extends \PHPUnit\Framework\TestCase
      * Test if the Page is not empty: The template should be displayed in the expected way.
      * Test if a form with input fields named firstname, lastname and message exists.
      */
-    public function testGetPage()
+    public function testGetPage(): void
     {
         $_SERVER["REQUEST_METHOD"] = "GET";
         $page = $this->runNormForm();
@@ -70,9 +72,9 @@ class NormFormTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * Test case: Form is sent without any data given
+     * Test case: Form is sent without any data given.
      */
-    public function testAllFieldsAreEmpty()
+    public function testAllFieldsAreEmpty(): void
     {
         $_SERVER["REQUEST_METHOD"] = "POST";
         $_POST = [NormFormDemo::FIRST_NAME => "", NormFormDemo::LAST_NAME => "", NormFormDemo::MESSAGE => ""];
@@ -82,21 +84,19 @@ class NormFormTest extends \PHPUnit\Framework\TestCase
             if ($param instanceof PostParameter) {
                 // Each input field has to be empty after page is returned
                 self::assertEmpty($param->getValue());
-            } else {
-                if ($param->getName() === "errorMessages") {
-                    // There are two required fields, that need a errorMessage associated
-                    self::assertCount(2, $param->getValue());
-                    self::assertArrayHasKey("firstname", $param->getValue());
-                    self::assertArrayHasKey("lastname", $param->getValue());
-                }
+            } elseif ($param->getName() === "errorMessages") {
+                // There are two required fields, that need a errorMessage associated
+                self::assertCount(2, $param->getValue());
+                self::assertArrayHasKey("firstname", $param->getValue());
+                self::assertArrayHasKey("lastname", $param->getValue());
             }
         }
     }
 
     /**
-     * Test case: Form is sent without no first name given
+     * Test case: Form is sent without no first name given.
      */
-    public function testFirstNameIsEmpty()
+    public function testFirstNameIsEmpty(): void
     {
         $_SERVER["REQUEST_METHOD"] = "POST";
         $_POST = [
@@ -113,19 +113,17 @@ class NormFormTest extends \PHPUnit\Framework\TestCase
                 } else {
                     self::assertNotEmpty($param->getValue());
                 }
-            } else {
-                if ($param instanceof GenericParameter) {
-                    self::assertCount(1, $param->getValue());
-                    self::assertArrayHasKey("firstname", $param->getValue());
-                }
+            } elseif ($param instanceof GenericParameter) {
+                self::assertCount(1, $param->getValue());
+                self::assertArrayHasKey("firstname", $param->getValue());
             }
         }
     }
 
     /**
-     * Test case: Form is sent with only first name given
+     * Test case: Form is sent with only first name given.
      */
-    public function testLastNameIsEmpty()
+    public function testLastNameIsEmpty(): void
     {
         $_SERVER["REQUEST_METHOD"] = "POST";
         $_POST = [NormFormDemo::FIRST_NAME => "NotEmpty", NormFormDemo::LAST_NAME => "", NormFormDemo::MESSAGE => ""];
@@ -138,11 +136,9 @@ class NormFormTest extends \PHPUnit\Framework\TestCase
                 } else {
                     self::assertEmpty($param->getValue());
                 }
-            } else {
-                if ($param instanceof GenericParameter) {
-                    self::assertCount(1, $param->getValue());
-                    self::assertArrayHasKey("lastname", $param->getValue());
-                }
+            } elseif ($param instanceof GenericParameter) {
+                self::assertCount(1, $param->getValue());
+                self::assertArrayHasKey("lastname", $param->getValue());
             }
         }
     }
@@ -150,7 +146,7 @@ class NormFormTest extends \PHPUnit\Framework\TestCase
     /**
      * Test case: Form is sent with all fields given. The result should be displayed
      */
-    public function testAllFieldsAreFilled()
+    public function testAllFieldsAreFilled(): void
     {
         $_SERVER["REQUEST_METHOD"] = "POST";
         $_POST = [
@@ -163,27 +159,27 @@ class NormFormTest extends \PHPUnit\Framework\TestCase
         foreach ($params as $param) {
             if ($param instanceof PostParameter) {
                 self::assertNotEmpty($param->getName());
-            } else {
-                if ($param instanceof GenericParameter) {
-                    if ($param->getName() === "errorMessages") {
-                        self::assertEmpty($param->getValue());
-                    }
-                    if ($param->getName() === "statusMessage") {
-                        self::assertNotEmpty($param->getValue());
-                    }
-                    if ($param->getName() === "result") {
-                        self::assertCount(3, $param->getValue());
-                    }
+            } elseif ($param instanceof GenericParameter) {
+                if ($param->getName() === "errorMessages") {
+                    self::assertEmpty($param->getValue());
+                }
+                if ($param->getName() === "statusMessage") {
+                    self::assertNotEmpty($param->getValue());
+                }
+                if ($param->getName() === "result") {
+                    self::assertCount(3, $param->getValue());
                 }
             }
         }
     }
 
+    /**
+     * Helper function to run the NormForm and capture the HTML output.
+     * @return false|string
+     */
     public function runNormForm()
     {
-        /**
-         * Capture the HTML output and do not send to terminal, but return to calling function.
-         */
+        // Capture the HTML output and do not send to terminal, but return to calling function.
         ob_start();
         $this->normForm->normform();
         $ret = ob_get_contents();
